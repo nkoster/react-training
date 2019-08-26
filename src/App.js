d793176e24adeb86fb6536839f74532abeda1e56
Niels Koster <n.koster@portavita.eu>
2019-08-27 00:11:57 +0200

    Set state stuff

--- src/App.js
+++ src/App.js
@@ -10,23 +10,38 @@ class App extends Component {
       { name: 'Henk', age: 41 }
     ]
   }
+
   switcher = () => {
     console.log('CLICK')
+    this.setState({
+      persons: [
+        { name: 'Niels', age: 50 },
+        { name: 'Laury', age: 48 },
+        { name: 'Petra', age: 30 } 
+      ]
+    })
   }
+
   render() {
     return (
       <div className="App">
         <h1>Aap!</h1>
         <button onClick={this.switcher}>Switch Name</button>
         <h3>okay</h3>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].name}>programmer</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].name} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}>programmer
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switcher} />
       </div>
-    );
-    // Under the hat:
-    // return React.createElement('div', {className: 'App'},
-    //        React.createElement('h1', null, 'Aaaap!'))
+    )
+
   }
 }
 
