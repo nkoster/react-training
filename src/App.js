d06bfb423c5135c949e0c88f70fbe8b08b799bf9
Niels Koster <n.koster@portavita.eu>
2019-08-26 22:58:09 +0200

    Use functional components instead of class based.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Niels', age: 50 },
       { name: 'Laury', age: 48 },
       { name: 'Henk', age: 41 }
-    ]
-  }
-  switcher = () => {
-    console.log('CLICK')
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Aap!</h1>
-        <button onClick={this.switcher}>Switch Name</button>
-        <h3>okay</h3>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].name}>programmer</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].name} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
-      </div>
-    );
-    // Under the hat:
-    // return React.createElement('div', {className: 'App'},
-    //        React.createElement('h1', null, 'Aaaap!'))
-  }
+    ],
+    otherState: 'some value'
+  })
+  console.log(personsState)
+  const switcher = () => {
+    setPersonsState({
+      persons: [
+        { name: 'Niels', age: 50 },
+        { name: 'Laury', age: 48 },
+        { name: 'Petra', age: 41 }
+      ],
+      otherState: personsState.otherState
+    })
+  }  
+  return (
+    <div className="App">
+      <h1>Aap!</h1>
+      <button onClick={switcher}>Switch Name</button>
+      <h3>okay</h3>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>programmer</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  )
 }
 
-export default App
+export default app
