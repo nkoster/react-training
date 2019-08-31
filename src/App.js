8dfa2f3d5124ba09fe1fbfabccee353fd5c9c623
Niels Koster <n.koster@portavita.eu>
2019-08-31 13:56:41 +0200

    Add show toggle function

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       { name: 'Niels', age: 50 },
       { name: 'Laury', age: 48 },
       { name: 'Henk', age: 41 }
-    ]
+    ],
+    showPersons: false
   }
 
   switcher = (newName) => {
@@ -32,6 +33,10 @@ class App extends Component {
     })
   }
 
+  toggle = () => {
+    this.setState({showPersons:!this.state.showPersons})
+  }
+
   render() {
     
     const style = {
@@ -44,24 +49,28 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Aap!</h1>
+        <h1>Appp!</h1>
         <button
-          onClick={() => this.switcher('Gijs')}
+          onClick={this.toggle}
           style={style}
         >Switch Name</button>
-        <h3>okay</h3>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}>programmer
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age} />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          click={this.switcher.bind(this, 'Apekop')}
-          changed={this.nameSwitch} />
+        { this.state.showPersons ?
+        <div>
+          <h3>okay</h3>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}>programmer
+          </Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age} />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+            click={this.switcher.bind(this, 'Apekop')}
+            changed={this.nameSwitch} />
+        </div>
+        : null }
       </div>
     )
 
