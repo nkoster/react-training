16614994d7acd7f300de071222fe101421eea5cb
Niels Koster <n.koster@portavita.eu>
2019-09-01 18:39:56 +0200

    Eyes

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ const person = props => (
 <div className="Person">
     <p onClick={props.click}>
         I'm {props.name} and I'm {props.age}
-        {props.children ? ` (${props.children})` : '__'}
+        {props.children ? ` (${props.children})` : ''}
     </p>
     <input type="text" onChange={props.changed} value={props.name} />
 </div>
