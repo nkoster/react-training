4f5788e9bfd1683ff63b3c432de3e642bdcbd93d
Niels Koster <n.koster@portavita.eu>
2019-09-03 23:08:55 +0200

    @media in component + some shit

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,14 +2,24 @@ import React from 'react'
 import Radium from 'radium'
 import './Person.css'
 
-const person = props => (
-<div className="Person">
-    <p onClick={props.click}>
-        I'm {props.name} and I'm {props.age}
-        {props.children ? ` (${props.children})` : ''}
-    </p>
-    <input type="text" onChange={props.changed} value={props.name} />
-</div>
-)
+const person = props => {
+
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
+
+    return (
+        <div className="Person">
+            <p onClick={props.click}>
+                I'm {props.name} and I'm {props.age}
+                {props.children ? ` (${props.children})` : ''}
+            </p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+
+}
 
 export default Radium(person)
