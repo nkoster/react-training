f6763a8ff343ec7ad36ba85397d9c3dd28bb75b4
Niels Koster <n.koster@portavita.eu>
2019-09-06 00:09:53 +0200

    getDerivedStateFromProps(props, state) shouldComponentUpdate(nextProps, nextState) examples

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react'
 import Person from './Person/Person'
 
 class Persons extends Component {
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('Persons.js] getDerivedStateFromProps', props)
+        return state
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Persons.js] shouldComponentUpdate', nextProps, nextState)
+        return true
+    }
+
     render() {
-        console.log('[Persons.js]', this.props.persons)
+        console.log('[Persons.js] render', this.props.persons)
         return this.props.persons.map((person, index) => 
             <Person
                 click={() => this.props.clicked(index)}
