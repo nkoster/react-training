79aa9f52da98e8edce15210f756c5e5ef02e898e
Niels Koster <n.koster@portavita.eu>
2019-09-05 23:25:50 +0200

    Add constructor() + super() + console.log lines to show components life cycle behavior

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,13 +4,27 @@ import Cockpit from '../components/Cockpit/Cockpit'
 import Persons from '../components/Persons/Persons'
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: 'a', name: 'Niels', age: 50 },
-      { id: 'b', name: 'Laury', age: 48 },
-      { id: 'c', name: 'Henk', age: 41 }
-    ],
-    showPersons: false
+
+  constructor(props) {
+    super(props)
+    console.log('[App.js] constructor')
+    this.state = {
+      persons: [
+        { id: 'a', name: 'Niels', age: 50 },
+        { id: 'b', name: 'Laury', age: 48 },
+        { id: 'c', name: 'Henk', age: 41 }
+      ],
+      showPersons: false
+    }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
   }
 
   updateName = (event, id) => {
@@ -39,6 +53,8 @@ class App extends Component {
   }
 
   render() {
+
+    console.log('[App.js] render')
     
     let persons = null
     if (this.state.showPersons) {
