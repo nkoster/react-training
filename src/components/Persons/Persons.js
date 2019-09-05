2054fdd7f352d9d9ece13d69a29b678687662b02
Niels Koster <n.koster@portavita.eu>
2019-09-05 23:49:15 +0200

    Initial

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Person from './Person/Person'
+
+class Persons extends Component {
+    render() {
+        console.log('[Persons.js]', this.props.persons)
+        return this.props.persons.map((person, index) => 
+            <Person
+                click={() => this.props.clicked(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.props.changed(event, person.id)}
+            />
+        )
+    }
+}
+
+export default Persons
