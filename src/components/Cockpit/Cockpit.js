bd7af30bc0ea15a516d7e00d204ecbc6bcc71eac
Niels Koster <n.koster@portavita.eu>
2019-09-05 21:42:19 +0200

    Move logic into Cockpit component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,1 +1,23 @@
-// bla
+import React from 'react'
+import classes from './Cockpit.module.css'
+
+const cockpit = props => {
+    const assignedClasses = []
+    let btnClass = ''
+    if (props.showPersons) {
+        btnClass = classes.Red
+    }
+    if (props.persons.length <=2 ) assignedClasses.push(classes.red)
+    if (props.persons.length <=1 ) assignedClasses.push(classes.bold)
+    return (
+        <div className={classes.Cockpit}>
+            <p className={assignedClasses.join(' ')}>Appp!</p>
+            <button
+            onClick={props.toggle}
+            className={btnClass}
+            >Switch Name</button>
+        </div>
+    )
+}
+
+export default cockpit
