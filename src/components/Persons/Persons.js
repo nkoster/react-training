2392bf6d8cd72722ec330076d05d3bf4b3e9f832
Niels Koster <n.koster@portavita.eu>
2019-09-08 16:45:20 +0200

    Replace shouldComponentUpdate for PureComponent stuff

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import Person from './Person/Person'
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
     // static getDerivedStateFromProps(props, state) {
     //     console.log('Persons.js] getDerivedStateFromProps', props)
@@ -12,14 +12,14 @@ class Persons extends Component {
     //     console.log('[Persons.js] componentWillReceiveProps', props)
     // }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        const update =
-            nextProps.persons !== this.props.persons || 
-            nextProps.changed !== this.props.changed ||
-            nextProps.clicked !== this.props.clicked
-        console.log('Persons.js] shouldComponentUpdate', update)
-        return update
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     const update =
+    //         nextProps.persons !== this.props.persons || 
+    //         nextProps.changed !== this.props.changed ||
+    //         nextProps.clicked !== this.props.clicked
+    //     console.log('Persons.js] shouldComponentUpdate', update)
+    //     return update
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
         console.log('Persons.js] getSnapshotBeforeUpdate', prevProps, prevState)
