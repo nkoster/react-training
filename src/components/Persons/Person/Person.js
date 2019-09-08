78482464e7e279265e54fbc07d7faddf7bc99f67
Niels Koster <n.koster@portavita.eu>
2019-09-08 21:43:00 +0200

    Replace Aux for React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import classes from './Person.module.css'
-import Aux from '../../../hoc/Aux'
+import React, { Component, Fragment } from 'react'
+// import classes from './Person.module.css'
+// import Aux from '../../../hoc/Aux'
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I'm {this.props.age}
                     {this.props.children ? ` (${this.props.children})` : ''}
@@ -16,7 +16,7 @@ class Person extends Component {
                     onChange={this.props.changed}
                     value={this.props.name}
                 />
-            </Aux>
+            </Fragment>
         )
     }
 }
