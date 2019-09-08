f6a1b121b73b3c81a02f36cf3de3cc63b0d23d0d
Niels Koster <n.koster@portavita.eu>
2019-09-08 21:13:13 +0200

    Return jsx as an array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,19 +4,21 @@ import classes from './Person.module.css'
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
-        return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>
+        return [
+
+                <p key="p" onClick={this.props.click}>
                     I'm {this.props.name} and I'm {this.props.age}
                     {this.props.children ? ` (${this.props.children})` : ''}
-                </p>
+                </p>,
+                <p key="p2" >{this.props.children}</p>,
                 <input
+                    key="input"
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}
                 />
-            </div>
-        )
+
+        ]
     }
 }
 
